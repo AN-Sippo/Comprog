5d79e4d24e67d4547637a8a794d3fb08f866bf48
AN-Sippo <oppis02.anzu@gmail.com>
2024-11-07 22:20:27 +0900

    bfsできれば十分じゃろ

---
+++ leetcode/116.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+# Definition for a Node.
+class Node:
+    def __init__(
+        self,
+        val: int = 0,
+        left: "Node" = None,
+        right: "Node" = None,
+        next: "Node" = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root):
+        if not root or root.left is None:
+            return root
+
+        prev = None
+        queue = deque([root])
+        while queue:
+            for _ in range(len(queue)):
+                current = queue.popleft()
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+                if prev:
+                    prev.next = current
+                prev = current
+            prev = None
+
+        return root
